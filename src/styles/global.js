a46d2bdc10ae6ce30b39e2d61e3cee4fa920a967
luamfmenezes <luamfmenezes@gmail.com>
2020-07-20 21:59:42 -0300

    first commit

---
+++ src/styles/global.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
+    *{
+        padding:0;
+        margin:0;
+        outline:0;
+        box-sizing:border-box;
+    }
+
+    html, body, #root{
+        height:100%;
+    }
+
+    body{
+        font: 14px 'Roboto';
+        color:#333;
+        background:#ecf1f8;
+        -webkit-font-smoothing: antialiased !important;
+    }
+    ul{
+        list-style:none;
+    }
+`;
